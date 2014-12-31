007f7b5a6d6d182f159a5876e89609070f5f21a3
BoD <BoD@JRAF.org>
2014-12-31 17:57:17 +0100

    - Color theme. - ScrollBar tweaks. - Other UI tweaks.

---
+++ mobile/src/main/java/org/jraf/android/digibod/handheld/app/addressinfo/list/AddressInfoCallbacks.java
@@ -0,0 +1,7 @@
+package org.jraf.android.digibod.handheld.app.addressinfo.list;
+
+import org.jraf.android.digibod.handheld.model.addressinfo.AddressInfo;
+
+public interface AddressInfoCallbacks {
+    void onAddressInfoClicked(AddressInfo addressInfo);
+}
